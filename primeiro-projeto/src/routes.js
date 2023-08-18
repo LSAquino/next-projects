4c0b4248014554c628c05f9513242bf869b61033
Lucas Silva Aquino <lucas.aquino@levty.com>
2023-08-18 00:47:27 -0300

    criando e estilizando meu primeiro projeto em react

---
+++ primeiro-projeto/src/routes.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repositorio from './pages/Repositorio';
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+            <switch>
+                <Route exact path="/" component={Main}/>
+                <Route exact path="/repositorio/:repositorio" component={Repositorio}/>
+            </switch>
+        
+        </BrowserRouter>
+    );
+}
